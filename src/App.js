2edc42ca8dc735661c648f73bc300d1e00efea67
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-05-23 19:31:54 +0430

    bit

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import TodoList from './components/TodoList';
 
 
 function App() {
+  
+  //  to handle the input and its value
   const [input, setInput] = useState("");
+  // for making the list of todos
   const [todos, setTodos]= useState([]);
 
   return (
     <div className="App">
     <header>List your Todo's</header>
-    {/*  props's are good things; use them */}
+    {/*  props's are good things; use them ^_^ */}
     <Form todos={todos}  setTodos={setTodos}  input={input} setInput={setInput} />
     <TodoList todos={todos}  setTodos={setTodos} />
     </div>
   );
+
 };
 
 export default App;
