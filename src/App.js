f9bbef2adf770cc48e6c9c7a351fdf19873a8b43
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-05-23 20:42:37 +0430

    later

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import TodoList from './components/TodoList';
 
 
 function App() {
-  
+
   //  to handle the input and its value
   const [input, setInput] = useState("");
   // for making the list of todos
   const [todos, setTodos]= useState([]);
+  // todo $$$$$$$$$$$ may change by backend operations $$$$$$$$$$$$$
+  // for editing the list of todos
+  const [editing, setEditiong]= useState(false);
 
   return (
     <div className="App">
