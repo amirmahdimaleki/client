5972a26682f5a92f370f2429f28e20010bb68f5d
amirDev00 <92927623+amirmahdimaleki@users.noreply.github.com>
2022-05-23 17:30:23 +0430

    todo

---
+++ src/components/Todo.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+const Todo = () => {
+   return(
+    <div className="todo"> af
+        <li className="todoItem"></li>
+
+        <button className="done-btn">
+            <i className="fas fa-check"></i>
+            </button>
+
+        <button className="delete-btn">
+            <i className="fas fa-trash"></i>
+            </button>
+    </div>
+   );
+};
+
+
+export default Todo;
